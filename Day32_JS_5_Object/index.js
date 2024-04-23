478d7f5ba6deaa3cfc2319aa15230c187dd1c6b6
Shreeom-Gurjar <141802307+Shreeom-Gurjar@users.noreply.github.com>
2024-04-23 17:23:32 +0530

    js

---
+++ Day32_JS_5_Object/index.js
@@ -0,0 +1,42 @@
+//console.log("Hello")
+/*const person1 = {
+    name:'Superman',
+    gmail:'Superman@gmail.com',
+    age:350,
+    salary:5,
+    panCard:'Qweinnddfjkm',
+    pincode:3456672
+
+}
+const person2={ ...person1,
+name:'batman',
+gmail:'batman@gmail.com',
+phone:899888999
+}
+
+//console.log("This is person",person1)
+//console.log("This is person",person2)
+const {
+    name, age, phone,salary} = person2
+    console.log("this is person2 object", age,phone,salary)*/
+  /*  const number1 = [10,20,30,40];
+    const number2 = [ ...number1,50,60,70]
+    console.log(number2)*/
+    /*const car = {
+        brandname:"BMW",
+        Wheels: 4,
+        enginetype: 'petol',
+    }
+   const Audi ={
+    ...car,name: superman , model: 2004
+   }
+   const RangeRover ={
+    ...car,color: blue ,
+   }
+   console.log(Audi)
+   console.log(RangeRover)*/
+   let a=10,b=20;
+   function sum(a,b){
+   console.log(a+b)
+   }
+   sum(10,20)
