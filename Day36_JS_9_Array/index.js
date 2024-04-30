2ccacbb2d4745513826bb4785864677452d67262
Shreeom-Gurjar <141802307+Shreeom-Gurjar@users.noreply.github.com>
2024-04-30 17:25:40 +0530

    js

---
+++ Day36_JS_9_Array/index.js
@@ -0,0 +1,43 @@
+// const arr = [11,44,21,43,12,"superman","spiderman","facebook",22.4,true,false,'a']
+// console.log(arr)
+// const movies = [
+//     {title:'Ra.one',Actor:'SRK',release:'2012',hit:false,flop:true},
+//     {title:'infinity war',Actor:'Salman',release:'2000',hit:true,flop:true},
+//     {title:'dhoom',Actor:'ritik',release:'2005',hit:true,flop:true},
+//     {title:'end game',Actor:'thonos',release:'2019',hit:false,flop:true},
+// ]
+// console.log(movies)
+// let i=0;
+// while(i<movies.length){
+// console.log(movies[i].title)
+// i++
+// }
+// for(let key in movies){
+//     console.log(key,movies[key])
+// }
+// for(let key of movies){
+//     console.log(element)
+// }
+// movies.forEach((value,index)=>console.log(value,index))
+const products = [
+    {title:'iphone',price:'90000',color:'black'}
+    {title:'mi',price:'11000',color:'white'}
+    {title:'vivo',price:'34000',color:'blue'}
+    {title:'nothing',price:'44000',color:'yellow'}
+]
+// console.log(products)
+// let i=0;
+// while(i<products.length){
+//     console.log(products[i].title)
+//     i++
+// }
+// for(let key in products){
+//          console.log(key,products[key])
+//     }
+// for(let key of products){
+//          console.log(element)
+//     }
+// products.forEach((value,index)=>console.log(value,index))
+// for(let =0;i<products.length;i++){
+//     console.log(products[i])
+// }
