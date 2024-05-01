79201ba2ac671a17c64b4486c29588a2423a7eec
Shreeom-Gurjar <141802307+Shreeom-Gurjar@users.noreply.github.com>
2024-05-01 17:39:45 +0530

    js

---
+++ Day37_JS_10_Array2/index.js
@@ -0,0 +1,73 @@
+const arr =[10,20,30,40,50,'superman','spiderman',20.99,false]
+ //console.log(arr)
+ // push = insert new element in the ending of arrary
+
+//  arr.push("before = ",arr)
+//  arr.push("batman")
+//  console.log("after = " ,arr)
+//pop = remove last element from array
+//  console.log("before = ",arr)
+//  arr.pop()
+//  console.log("after = " ,arr)
+ //unshift = add new element at begging of the array
+//  console.log("before = ",arr)
+//  arr.unshift('shriom')
+//  console.log("after = " ,arr)
+ // shift = remove begging element from array
+ //console.log("before = ",arr)
+ //arr.shift()
+//  console.log("before = " ,arr)
+//  for(i=0;i<4;i++){
+//     console.log(i)
+//     arr.shift()
+//  }
+//  console.log('after =' ,arr)
+// while(arr.length !=0){
+//     arr.shift();
+// }
+// console.log("before = " ,arr)
+// arr.splice(4,3)
+// console.log('after =' ,arr)
+// console.log("before = " ,arr)
+//  const result = arr.slice(4,7)
+// console.log('resulted array =' ,result)
+const salary = [
+    {emp:1,salary:100,name:'superman'},
+    {emp:2,salary:200,name:'spiderman'},
+    {emp:3,salary:300,name:'batman'},
+    {emp:4,salary:400,name:'thor'},
+    {emp:5,salary:500,name:'captain'},
+    {emp:6,salary:600,name:'plyer'},
+    {emp:7,salary:700,name:'ironman'},
+    {emp:8,salary:800,name:'tokyo'},
+    {emp:9,salary:900,name:'professor'},
+];
+
+let evenSalaryEmp = []
+for(let element of salary){
+    if(element.salary %2 ==0){
+     evenSalaryEmp.push(element)
+}
+}
+console.log("evenSalary array",evenSalaryEmp)
+
+// if(Element.SALARY> 300 && ELEMENT .SALARY <750)
+// evenSalaryEmp.push(element)
+
+
+
+
+
+
+
+
+
+// let result = salary.slice(3,9)
+// console.log(result)
+// let salarySum =0
+// // for(let element of result) salarySum + element.salary
+// // console.log(salarySum)
+// for(let i=0;i<result.length;i++){
+//     salarySum = salarySum + result[i].salary
+// }
+// console.log(salarySum)
