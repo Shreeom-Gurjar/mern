d0aa554169906e4e89d2d4687438d9d3d3837724
Shreeom-Gurjar <141802307+Shreeom-Gurjar@users.noreply.github.com>
2024-05-02 17:14:15 +0530

    js

---
+++ Day38_JS_11_Array/index.js
@@ -0,0 +1,43 @@
+// const arr =[10,20,30,40,"superman",'spiderman','thor','hulk','chennai express']
+// const mySlice = (start,end)=>{
+//     const temp = [];
+//     for(let i=start;i<end;i++){
+//         temp.push(arr[i])
+//     }
+//     return temp;
+// }
+// console.log("originalslice slice " arr.slice(5,7))
+// console.log("apna slice ",mySlice(5,7))
+
+
+
+
+// const bollywood = ["pk","3 - idiots","Shaadi me jaroor aana","12th fail"]
+// const southIndian =["robot","salaar","bahubali","pushpa"]
+// const allMovies = bollywood.concat(southIndian)
+// console.log(allMovies)
+
+
+
+// array searching methods
+//indexoff
+// const phones = ["iphone","oneplus","realme","redmi","sony xperia","samsung"]
+// console.log(phones.indexOf("redmi"))
+
+//
+// const phones = ["iphone","oneplus","realme","redmi","sony xperia","samsung","redmi"]
+// console.log(phones.lastIndexOf("redmi"))
+
+
+//
+// const phones = ["iphone","oneplus","realme","sony xperia","samsung","redmi"]
+// console.log(phones.includes("vivo"))
+
+//
+// const phones = ["iphone","oneplus","realme","redmi","sony xperia","samsung","redmi","asus","blackberry"]
+// console.log(phones.sort())
+
+
+//
+// const number =[92222,143,43,3112,222,700,655,82,0]
+// console.log(number.sort((a,b)=>b-a))
