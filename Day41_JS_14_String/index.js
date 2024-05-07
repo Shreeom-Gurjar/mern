97b9d5c5c23125f98fedaa8b3eb00c2b7fae6e96
Shreeom-Gurjar <141802307+Shreeom-Gurjar@users.noreply.github.com>
2024-05-07 17:20:57 +0530

    string

---
+++ Day41_JS_14_String/index.js
@@ -0,0 +1,17 @@
+const str = "superman"
+//console.log(str)
+
+
+//to change the string to upperCase
+// const updatedString = str.toUpperCase()
+
+
+// console.log(updatedString)
+//to change the string to lowercase
+
+
+// const updatedString = str.toLowerCase()
+// console.log(updatedString)
+
+const str = "superman   shrma ji   "
+console.log(str.trim())
